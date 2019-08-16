31ebc225bc6698a8b994aae881bbc3c407255f6d
hacpaka <hacpaka@eggbe.com>
2019-08-16 22:46:49 +0300

    +

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,14 +1,13 @@
 <?php
-
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel {
+
 	/**
 	 * The Artisan commands provided by your application.
-	 *
 	 * @var array
 	 */
 	protected $commands = [
@@ -17,23 +16,23 @@ class Kernel extends ConsoleKernel {
 
 	/**
 	 * Define the application's command schedule.
-	 *
-	 * @param \Illuminate\Console\Scheduling\Schedule $schedule
+	 * @param Schedule $schedule
 	 * @return void
 	 */
 	protected function schedule(Schedule $schedule) {
-		// $schedule->command('inspire')
-		//          ->hourly();
+		/*
+		 * The schedule is empty.
+		 *
+		 * The example:
+		 * $schedule->command('commandname')->hourly();
+		*/
 	}
 
 	/**
 	 * Register the commands for the application.
-	 *
 	 * @return void
 	 */
 	protected function commands() {
 		$this->load(__DIR__ . '/Commands');
-
-		require base_path('routes/console.php');
 	}
 }
