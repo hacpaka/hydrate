0c80a1f69c5c99fcfe758660ce51212b35a1431a
hacpaka <hacpaka@eggbe.com>
2019-08-19 13:04:53 +0300

    Middlewares moved

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,10 +26,10 @@ class Kernel extends HttpKernel {
 	 * @var array
 	 */
 	protected $middleware = [
-		\App\Http\Middleware\TrustProxies::class,
-		\App\Http\Middleware\CheckForMaintenanceMode::class,
+		\App\Middleware\TrustProxies::class,
+		\App\Middleware\CheckForMaintenanceMode::class,
 		\Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-		\App\Http\Middleware\TrimStrings::class,
+		\App\Middleware\TrimStrings::class,
 		\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 	];
 
@@ -40,12 +40,12 @@ class Kernel extends HttpKernel {
 	 */
 	protected $middlewareGroups = [
 		'web' => [
-			\App\Http\Middleware\EncryptCookies::class,
+			\App\Middleware\EncryptCookies::class,
 			\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
 			\Illuminate\Session\Middleware\StartSession::class,
 			// \Illuminate\Session\Middleware\AuthenticateSession::class,
 			\Illuminate\View\Middleware\ShareErrorsFromSession::class,
-			\App\Http\Middleware\VerifyCsrfToken::class,
+			\App\Middleware\VerifyCsrfToken::class,
 			\Illuminate\Routing\Middleware\SubstituteBindings::class,
 		],
 
@@ -62,12 +62,12 @@ class Kernel extends HttpKernel {
 	 * @var array
 	 */
 	protected $routeMiddleware = [
-		'auth' => \App\Http\Middleware\Authenticate::class,
+		'auth' => \App\Middleware\Authenticate::class,
 		'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
 		'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
 		'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
 		'can' => \Illuminate\Auth\Middleware\Authorize::class,
-		'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+		'guest' => \App\Middleware\RedirectIfAuthenticated::class,
 		'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
 		'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
 		'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
@@ -76,7 +76,6 @@ class Kernel extends HttpKernel {
 
 	/**
 	 * The priority-sorted list of middleware.
-	 *
 	 * This forces non-global middleware to always be in the given order.
 	 *
 	 * @var array
@@ -84,7 +83,7 @@ class Kernel extends HttpKernel {
 	protected $middlewarePriority = [
 		\Illuminate\Session\Middleware\StartSession::class,
 		\Illuminate\View\Middleware\ShareErrorsFromSession::class,
-		\App\Http\Middleware\Authenticate::class,
+		\App\Middleware\Authenticate::class,
 		\Illuminate\Routing\Middleware\ThrottleRequests::class,
 		\Illuminate\Session\Middleware\AuthenticateSession::class,
 		\Illuminate\Routing\Middleware\SubstituteBindings::class,
