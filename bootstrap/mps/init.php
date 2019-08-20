124fc4b2309658c33bda96d0837760d273d1dc39
hacpaka <hacpaka@eggbe.com>
2019-08-20 08:35:04 +0300

    +

--- bootstrap/mps/init.php
+++ bootstrap/mps/init.php
@@ -18,21 +18,24 @@ try {
 	 * the unit base directory has to be a subfolder
 	 * of the application units folder.
 	 */
-	define('UNIT_NAME', call_user_func(function (){ if (!preg_match('/^' . preg_quote(__DIR__ . DIRECTORY_SEPARATOR . 'units'
-		. DIRECTORY_SEPARATOR, '/') . '(.*)$/', rtrim(UNIT_PATH, DIRECTORY_SEPARATOR), $Matches)){
-			throw new \Exception('Unit path is not a subfolder of the main directory!'); }
+	define('UNIT_NAME', call_user_func(function () {
+		if (!preg_match('/^' . preg_quote(realpath(__DIR__
+			. '/../../units/')  . DIRECTORY_SEPARATOR, '/') . '(.*)$/', UNIT_PATH, $Matches)) {
+
+			throw new \Exception('Unit path is not a subfolder of the main directory!');
+		}
 
 		/**
 		 * Actually, the unit name it's the unit subfolder
 		 * with some limitation: only Latin letters,
 		 * underscore or dash characters.
 		 */
-		if (!preg_match('/^[A-Z0-9_-]/i', $Matches[1])){
+		if (!preg_match('/^[A-Z0-9_-]/i', $Matches[1])) {
 			throw new \Exception(sprintf('Invalid unit name: %s!', $Matches[1]));
 		}
 
 		return $Matches[1];
 	}));
-}catch (\Throwable $Exception){
+} catch (\Throwable $Exception) {
 	trigger_error($Exception->getMessage(), E_USER_ERROR);
 }
