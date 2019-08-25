49ea5674aa6c5b0f8d7f9dc5a66142c89a081efc
hacpaka <hacpaka@eggbe.com>
2019-08-25 08:07:42 +0300

    +

---
+++ units/welcome/app/Controllers/WelcomeController.php
@@ -0,0 +1,22 @@
+<?php
+namespace Units\Dashboard\Controllers;
+
+use \Illuminate\Routing\Controller;
+
+use \Illuminate\Foundation\Bus\DispatchesJobs;
+use \Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use \Illuminate\Foundation\Validation\ValidatesRequests;
+
+use \Illuminate\View\View;
+
+class WelcomeController extends Controller {
+	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+	/**
+	 * @return View
+	 */
+	public final function main(){
+		return view('welcom.blade.php');
+	}
+}
+
