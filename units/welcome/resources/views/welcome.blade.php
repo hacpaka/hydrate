a040a04b8c7b87400aa12b868212e69959b73dc0
hacpaka <hacpaka@eggbe.com>
2019-09-04 19:22:27 +0300

    +

--- units/welcome/resources/views/welcome.blade.php
+++ units/welcome/resources/views/welcome.blade.php
@@ -12,12 +12,16 @@
 	<!-- Styles -->
 	<style>
 		html, body {
-			background-color: #fff;
-			color: #636b6f;
+			background-color: #636b6f;
+			color: #ffffff;
 			font-family: 'Nunito', sans-serif;
 			font-weight: 200;
 			height: 100vh;
 			margin: 0;
+		}
+
+		body * {
+			box-sizing: border-box;
 		}
 
 		.full-height {
@@ -45,11 +49,44 @@
 		}
 
 		.title {
+			display: inline-block;
+			height: 120px;
+			line-height: 120px;
+		}
+
+		.title > span {
 			font-size: 84px;
+			display: inline-block;
+			position: relative;
+			padding: 0 120px 0 0;
+			height: 120px;
+			line-height: 120px;
+		}
+
+		.title > span > a,
+		.title > span > a:hover,
+		.title > span > a:active,
+		.title > span > a:visited {
+			display: block;
+			height: 38px;
+			line-height: 38px;
+			margin: 30px 0;
+			font-size: 28px;
+			background-color: rgba(152, 252, 152, 0.7);
+			color: #ffffff;
+			text-decoration: none;
+			border-radius: 5px;
+			position: absolute;
+			top: 0;
+			right: 0;
+			width: 100px;
+			text-align: center;
+			font-weight: bold;
+			cursor: pointer;
 		}
 
 		.links > a {
-			color: #636b6f;
+			color: #ffffff;
 			padding: 0 25px;
 			font-size: 13px;
 			font-weight: 600;
@@ -81,7 +118,11 @@
 
 	<div class="content">
 		<div class="title m-b-md">
-			Laravel
+			<span>
+				Laravel
+
+				<a href="https://github.com/hacpaka/laravel-mps">MPS</a>
+			</span>
 		</div>
 
 		<div class="links">
