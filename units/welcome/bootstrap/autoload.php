3f9eee248ce42ac1557fd9751494df3cf213614f
hacpaka <hacpaka@eggbe.com>
2019-09-05 12:19:47 +0300

    +

---
+++ units/welcome/bootstrap/autoload.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Unit Functions
+|--------------------------------------------------------------------------
+|
+| These functions are unique for a unit.
+| They are going to be loaded automatically during the startup process.
+|
+*/
